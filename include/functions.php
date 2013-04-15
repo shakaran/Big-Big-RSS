f9061100f9afd0fccfa403af506d4a828caf14ba
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-04-15 14:54:17 +0400

    fix schema for favicon_avg_color
    Conflicts:
    	include/functions.php

--- include/functions.php
+++ include/functions.php
@@ -1,6 +1,4 @@
 <?php
-define('EXPECTED_CONFIG_VERSION', 26);
-
 if(file_exists('../conf/Config.php'))
 {
     require_once '../conf/Config.php';
@@ -10,7 +8,8 @@ else
     require_once 'conf/Config.php';
 }
 
-	define('SCHEMA_VERSION', 116);
+	define('EXPECTED_CONFIG_VERSION', 26);
+	define('SCHEMA_VERSION', 117);
 
 	define('LABEL_BASE_INDEX', -1024);
 	define('PLUGIN_FEED_BASE_INDEX', -128);
@@ -543,13 +542,13 @@ else
             return $icon_file;
 		}
 	}
-	
+
 	function calculate_avg_color($iconFile) {
-		
+
 		require_once "lib/floIcon.php";
-		
+
 		$imgInfo = @getimagesize($iconFile);
-        
+
 		if(strtolower($imgInfo['mime'])=='image/vnd.microsoft.icon') {
 			$ico = new floIcon();
 			@$ico->readICO($iconFile);
