1db163e30812fda176cee5eb6f952603829d3e49
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-07-04 12:45:48 +0200

    Update typo references in table name var

--- core/lib/Feed.php
+++ core/lib/Feed.php
@@ -23,6 +23,9 @@ class Feed
 	 * select (limited) if the first method is not available.
 	 * 
 	 * @param string $database_driver The driverdatabase type for queries
+	 * 
+	 * @author Ángel Guzmán Maeso <shakaran@gmail.com>
+	 * 
 	 * @return boolean TRUE if the table exists, or FALSE on error
 	 */
 	public static function existLegacyTable($database_driver = NULL)
@@ -30,7 +33,7 @@ class Feed
 		global $link;
 
 		// Try to search the table with first legacy detection method
-		$result = db_query($link, "SHOW TABLES LIKE '" . self::table_name . "'", $database_driver, FALSE);
+		$result = db_query($link, "SHOW TABLES LIKE '" . self::$table_name . "'", $database_driver, FALSE);
 		
 		if(db_num_rows($result) == 1)
 		{
@@ -39,7 +42,7 @@ class Feed
 		else 
 		{
 			// If there are restrictions or some problem, try a simple select method (with limited rows for avoid overhead)
-			$result = db_query($link, "SELECT TRUE FROM " . self::table_name . " LIMIT 1", $database_driver, FALSE);
+			$result = db_query($link, "SELECT TRUE FROM " . self::$table_name . " LIMIT 1", $database_driver, FALSE);
 			
 			return db_num_rows($result) == 1;
 		}
